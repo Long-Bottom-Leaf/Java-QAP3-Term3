f633f6abd2b4f8618bbc3a59a90eccedaf7ff4b2
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2025-11-12 20:54:41 -0330

    Demo class and results image added

--- Problem2/Demo.java
+++ Problem2/Demo.java
@@ -10,14 +10,14 @@ public class Demo {
     public static void main(String[] args) {
         // Create a Point object
         Point p1 = new Point(2.5f, 3.5f);
-        System.out.println("Point: " + p1);
+        System.out.println("\n Point: " + p1);
 
         // Create a MovablePoint object
         MovablePoint mp1 = new MovablePoint(2.5f, 3.5f, 1.2f, 2.3f);
-        System.out.println("Before move: " + mp1);
+        System.out.println("\n Before move: " + mp1);
 
         // Move the point and display the new position
         mp1.move();
-        System.out.println("After move: " + mp1);
+        System.out.println("\n After move: " + mp1);
     }
 }
